9880754e5a2eeaa65c26eba4b1a12a1b67c6c72e
Desber12 <145825644+Desber12@users.noreply.github.com>
2025-07-15 16:32:52 +0700

    update

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-black leading-tight">
             Daftar Pesanan
         </h2>
     </x-slot>
@@ -14,7 +14,7 @@
 
         <div class="mb-4">
             <a href="{{ route('orders.create') }}"
-               class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+               class="px-4 py-2 bg-red-500 text-white rounded">
                 Tambah Pesanan
             </a>
         </div>
@@ -22,20 +22,21 @@
         <table class="w-full border border-gray-300">
             <thead>
                 <tr class="bg-gray-100">
-                    <th class="border px-4 py-2">ID</th>
-                    <th class="border px-4 py-2">Nama User</th>
-                    <th class="border px-4 py-2">Total Harga</th>
-                    <th class="border px-4 py-2">Status</th>
+                    <th class="border border-black px-4 py-2">ID</th>
+                    <th class="border border-black px-4 py-2">Nama User</th>
+                    <th class="border border-black px-4 py-2">Total Harga</th>
+                    <th class="border border-black px-4 py-2">Status</th>
+                    <th class="border border-black px-4 py-2">Tanggal Transaksi</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($orders as $order)
                     <tr>
-                        <td class="border px-4 py-2">{{ $order->id }}</td>
-                        <td class="border px-4 py-2">{{ $order->user->name }}</td>
-                        <td class="border px-4 py-2">Rp {{ number_format($order->total_harga, 0, ',', '.') }}</td>
-                        <td class="border px-4 py-2">{{ ucfirst($order->status) }}</td>
-                        <td class="border px-4 py-2">{{ $order->created_at->format('d M Y') }}</td>
+                        <td class="border border-black px-4 py-2">{{ $order->id }}</td>
+                        <td class="border border-black px-4 py-2">{{ $order->user->name }}</td>
+                        <td class="border border-black px-4 py-2">Rp {{ number_format($order->total_harga, 0, ',', '.') }}</td>
+                        <td class="border border-black px-4 py-2">{{ ucfirst($order->status) }}</td>
+                        <td class="border border-black px-4 py-2">{{ $order->created_at->format('d M Y') }}</td>
                     </tr>
                 @endforeach
             </tbody>
